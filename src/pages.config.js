ce3b3c60387edf77b29e16d67e2c1ec2f6c25197
heylilyyoursofun <heylilyyoursofun@gmail.com>
2025-12-05 01:05:37 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,11 +1,13 @@
 import Home from './pages/Home';
 import GenerateImages from './pages/GenerateImages';
+import GenerateImagesNow from './pages/GenerateImagesNow';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Home": Home,
     "GenerateImages": GenerateImages,
+    "GenerateImagesNow": GenerateImagesNow,
 }
 
 export const pagesConfig = {
